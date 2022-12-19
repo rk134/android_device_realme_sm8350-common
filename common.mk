2585d9db74c8118ec5e671c7a2d7e48582954356
Sourajit Karmakar <sourajitk@statixos.com>
2022-12-19 12:19:54 +0800

    sm8350-common: Switch to our conditional to enable blur
    Change-Id: I265ef10eaeb2755d4c20dea596a8462d11628e61
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

--- common.mk
+++ common.mk
@@ -103,6 +103,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Blur
+TARGET_USES_BLUR := true
+
 # Board
 PRODUCT_USES_QCOM_HARDWARE := true
 
