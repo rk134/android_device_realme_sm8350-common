StatiXOS/android_device_oneplus_sm8350-common

sm8350-common: Move over to prebuilt AOSP way of bundling kernel
In the future, we plan on completely getting rid of inline
kernel building. This commit aims to completely  eliminate
any dependencies on our custom kernel building framework.

Change-Id: I8947c7106898c4fffb240a9ea85ea61c4c7399e6
Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

common.mk
@@ -247,6 +247,15 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     ueventd.qcom.rc
 
+# Kernel
+TARGET_KERNEL_DIR ?= device/oneplus/sm8350-kernel
+LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image
+
+PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
+
+# Kernel Headers
+PRODUCT_VENDOR_KERNEL_HEADERS += device/oneplus/sm8350-kernel/kernel-headers
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor

rk134/android_device_realme_sm8350-common

Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

common.mk
@@ -243,6 +243,15 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     ueventd.qcom.rc
 
+# Kernel
+TARGET_KERNEL_DIR ?= device/realme/sm8350-kernel
+LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image
+
+PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
+
+# Kernel Headers
+PRODUCT_VENDOR_KERNEL_HEADERS += device/realme/sm8350-kernel/kernel-headers
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor